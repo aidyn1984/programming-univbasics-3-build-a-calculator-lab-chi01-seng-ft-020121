fc4fbd664febc15e659e5b110122276cccbc9929
Val_1 S_z <csa.aiden@gmail.com>
2021-01-22 08:11:19 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  answer = num1.to_i + num2.to_i
+  return answer
 end
 
 def subtraction(num1, num2)
-
+  answer = num1.to_i - num2.to_i
+  return answer
 end
 
 def division(num1, num2)
-
+  answer = num1.to_i / num2.to_i
+  return answer
 end
 
 def multiplication(num1, num2)
-
+  answer = num1.to_i * num2.to_i
+  return answer
 end
 
 def modulo(num1, num2)
-
+  answer = num1.to_i % num2.to_i
+  return answer
 end
 
 def square_root(num)
-
+  answer = Integer.sqrt(num)
+  return answer
 end
